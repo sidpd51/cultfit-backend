1accca29b11197216e2093ab0ef65b6be6422a89
sidpd51 <sidpd510@gmail.com>
2025-05-04 04:06:14 +0530

    feat: add request body validation for user creation and update endpoints

--- src/routers/v1/user.router.ts
+++ src/routers/v1/user.router.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import { createUserHandler, getAllUsers, updateUser } from '../../controllers/user.controller';
 import { validateRequetBody } from '../../validators';
-import { updateUserSchema } from '../../validators/user.validator';
+import { createUserSchema, updateUserSchema } from '../../validators/user.validator';
 
 const userRouter = express.Router();
 
 userRouter.get('/', getAllUsers);
-userRouter.post('/', createUserHandler);
+userRouter.post('/', validateRequetBody(createUserSchema), createUserHandler);
 userRouter.patch('/:id', validateRequetBody(updateUserSchema), updateUser);
 
 export default userRouter;
--- src/validators/user.validator.ts
+++ src/validators/user.validator.ts
@@ -2,7 +2,6 @@ import { z } from "zod";
 
 const e164PhoneRegex = /^\+[1-9]\d{9,14}$/;
 const strongPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-const userRoleEnum = ["user", "admin"] as const;
 
 export const createUserSchema = z.object({
     name: z.string({
@@ -11,23 +10,19 @@ export const createUserSchema = z.object({
     email: z.string({
         required_error: "Email is required",
     }).email("Invalid email address"),
-    phone: z.string({
+    phoneNumber: z.string({
         required_error: "Phone is required",
     }).regex(e164PhoneRegex, "Phone number must be in E.164 format"),
     password: z.string({
         required_error: "Password is required",
     }).regex(strongPasswordRegex, "Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character"),
-    role: z.enum(userRoleEnum, {
-        required_error: "Role is required",
-        invalid_type_error: "Role must be either 'user' or 'admin'",
-    })
 });
 
 export const updateUserSchema = z.object({
     name: z.string({
         required_error: "Name is required",
     }).min(1, "Name should alleast have one character"),
-    phone: z.string({
+    phoneNumber: z.string({
         required_error: "Phone is required",
     }).regex(e164PhoneRegex, "Phone number must be in E.164 format"),
     password: z.string({
