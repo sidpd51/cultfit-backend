2107a5b7c855677eb4f03a17df4c34f4e80cf95b
sidpd51 <sidpd510@gmail.com>
2025-05-04 01:54:28 +0530

    feat: add Sequelize model for User with validation and associations

---
+++ src/db/models/user.ts
@@ -0,0 +1,79 @@
+import { CreationOptional, DataTypes, InferAttributes, InferCreationAttributes, Model } from "sequelize";
+import sequelize from "./sequelize";
+
+class User extends Model<InferAttributes<User>, InferCreationAttributes<User>> {
+    declare id: CreationOptional<number>;
+    declare name: string;
+    declare email: string;
+    declare phoneNumber: CreationOptional<string>;
+    declare password: CreationOptional<string>;
+    declare strikeCount: CreationOptional<number>;
+    declare noStrikeCount: CreationOptional<number>;
+    declare bannedUntil: CreationOptional<Date>;
+    declare createdAt: CreationOptional<Date>;
+    declare updatedAt: CreationOptional<Date>;
+}
+
+User.init({
+    id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+    },
+    phoneNumber: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        unique: true,
+        validate: {
+            is: /^\+?[1-9]\d{1,14}$/
+        }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    strikeCount: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0
+    },
+    noStrikeCount: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0
+    },
+    bannedUntil: {
+        type: DataTypes.DATE,
+        allowNull: true,
+        defaultValue: null,
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+    }
+}, {
+    sequelize: sequelize,
+    tableName: "users",
+    timestamps: true,
+    underscored: true,
+})
+
+export default User;
